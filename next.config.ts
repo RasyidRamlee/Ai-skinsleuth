b005d1ebb9ab7d845655405fa390305a0d8b9aad
RasyidRamlee <rasyidramlee0511@gmail.com>
2025-09-15 04:16:01 +0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
+// next.config.ts
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://ai-skinsleuth-85a14.web.app/:path*', // <-- your Studio domain
+        permanent: true, // 308
+      },
+    ];
+  },
 };
 
 export default nextConfig;
